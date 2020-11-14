ca860268118090174e4978a3de677f0c25aa1ba1
j65864ap <andrii.perehuda@student.manchester.ac.uk>
2020-11-14 12:34:30 +0000

    Add test to check if the defender has HeavyStatus
    A test to check is the attacked player has HeavyStatus after being
    attacked by the wandOfSluggishness.

--- tests/games/stendhal/server/entity/item/WandOfSluggishnessTest.java
+++ tests/games/stendhal/server/entity/item/WandOfSluggishnessTest.java
@@ -3,14 +3,28 @@ package games.stendhal.server.entity.item;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rp.StendhalRPAction;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.status.StatusType;
 import games.stendhal.server.maps.MockStendhalRPRuleProcessor;
+import games.stendhal.server.maps.MockStendlRPWorld;
 import utilities.PlayerTestHelper;
 
 public class WandOfSluggishnessTest {
+	private StendhalRPZone zone;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		zone = new StendhalRPZone("zone", 20, 20);
+		MockStendlRPWorld.get().addRPZone(zone);
+	}
+	
 	
 	@After
 	public void tearDown() throws Exception {
@@ -28,6 +42,36 @@ public class WandOfSluggishnessTest {
 
 		player.equip("bag", item);
 		assertTrue(player.isEquipped("wandOfSluggishness"));
+	}
+	
+	
+	/*
+	 * 
+	 */
+	@Test
+	public void testDefenderSlowedDownAFterAttack() {
+		final Player player = PlayerTestHelper.createPlayer("testPlayer");
+		final Player victim = PlayerTestHelper.createPlayer("testVictim");
+		zone.add(player);
+		zone.add(victim);
+		
+		Item item = SingletonRepository.getEntityManager().getItem("wandOfSluggishness");
+		
+		player.equip("lhand", item);
+		StendhalRPAction.startAttack(player, victim);
+		StendhalRPAction.playerAttack(player, victim);
+		
+		StatusType statusType = StatusType.HEAVY;
+		
+		assertTrue(victim.hasStatus(statusType));
+		player.stopAttack();
+		
+	}
+	
+	@Test
+	public void testWandIsAnItem() {
+		Item item = SingletonRepository.getEntityManager().getItem("wandOfSluggishness");
+		assertNotNull(item);
 	}
 
 }
