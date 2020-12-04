ad8119de362b0a93c214f9bcc48d5236bffa9cf3
Amish Shah <amish.shah@student.manchester.ac.uk>
2020-12-04 21:12:09 +0000

    Implement tests for AdminNote
    Adds a test for AdminNote to make sure that
    it functions as expected.

---
+++ tests/games/stendhal/client/actions/AdminNoteTest.java
@@ -0,0 +1,75 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.client.actions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.client.MockStendhalClient;
+import games.stendhal.client.StendhalClient;
+import marauroa.common.game.RPAction;
+
+public class AdminNoteTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		SlashActionRepository.register();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		StendhalClient.resetClient();
+	}
+
+	/**
+	 * Tests for execute.
+	 */
+	@Test
+	public void testExecute() {
+		new MockStendhalClient() {
+			@Override
+			public void send(final RPAction action) {
+				assertEquals("adminnote", action.get("type"));
+				assertEquals("kash", action.get("target"));
+				assertEquals("cool and fun times", action.get("note"));
+			}
+		};
+		final SlashAction action = SlashActionRepository.get("adminnote");
+		assertTrue(action.execute(new String []{"kash"}, "cool and fun times"));
+	}
+
+	/**
+	 * Tests for getMaximumParameters.
+	 */
+	@Test
+	public void testGetMaximumParameters() {
+		final SlashAction action = SlashActionRepository.get("adminnote");
+		assertThat(action.getMaximumParameters(), is(1));
+	}
+
+	/**
+	 * Tests for getMinimumParameters.
+	 */
+	@Test
+	public void testGetMinimumParameters() {
+		final SlashAction action = SlashActionRepository.get("adminnote");
+		assertThat(action.getMinimumParameters(), is(1));
+	}
+
+}
