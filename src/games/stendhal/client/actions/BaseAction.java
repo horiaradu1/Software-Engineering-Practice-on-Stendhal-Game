a4c6d13c06f301d3719989ed9f7b9eb273736ae3
j65864ap <andrii.perehuda@student.manchester.ac.uk>
2020-12-05 15:50:51 +0000

    Add BaseAction class
    Created BaseAction class which implements the SlashAction interface.

---
+++ src/games/stendhal/client/actions/BaseAction.java
@@ -0,0 +1,86 @@
+package games.stendhal.client.actions;
+
+import java.util.Map;
+
+import games.stendhal.client.ClientSingletonRepository;
+import marauroa.common.game.RPAction;
+
+public class BaseAction implements SlashAction {
+	
+	private int minimumParameters;
+	private int maximumParameters;
+	
+	private boolean remainderRequired = false;
+	private boolean remainderNonEmpty = false;
+	
+	private Map<String, String> parameters, staticParameters;
+	
+	protected BaseAction(int minParams, int maxParams, Map<String, String> parameters, Map<String, String> staticParameters) {
+		minimumParameters = minParams;
+		maximumParameters = maxParams;
+		this.parameters = parameters;
+		this.staticParameters = staticParameters;
+	}
+
+	@Override
+	public boolean execute(String[] params, String remainder) {
+		
+		if (remainderRequired && remainder.isEmpty()) {
+			return false;
+		}
+		
+		final RPAction action = new RPAction();
+		
+		for (Map.Entry<String, String> entry : parameters.entrySet()) {
+			 String key = entry.getKey();
+			 String value = entry.getValue();
+
+			 if (value.equals("remainder")) {
+                 if (!(remainderNonEmpty && remainder.isEmpty())) {
+                     action.put(key, remainder);
+                 }
+             } else {
+                 int index = Integer.parseInt(value);
+                 if (index > minimumParameters) {
+                     if ((params.length > index) && (params[index] != null)) {
+                         action.put(key, params[index]);
+                     }
+                 } else {
+                     action.put(key, params[index]);
+                 }
+             }
+		}
+		
+		for (Map.Entry<String, String> entry : staticParameters.entrySet()) {
+			 String key = entry.getKey();
+			 String value = entry.getValue();
+
+			 action.put(key, value);
+		}
+
+
+		ClientSingletonRepository.getClientFramework().send(action);
+
+		return true;
+	}
+	
+	
+
+	@Override
+	public int getMaximumParameters() {
+		return maximumParameters;
+	}
+
+	@Override
+	public int getMinimumParameters() {
+		return minimumParameters;
+	}
+	
+	public void setRemainderRequired(boolean b) {
+		remainderRequired = b;
+	}
+	
+	public void setRemainderNonEmpty(boolean b) {
+		remainderNonEmpty = b;
+	}
+}
